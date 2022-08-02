41f1f1429607e110bf45330d66a16190f5038c29
Aplexas99 <toni_dzoic@hotmail.com>
2022-08-02 02:04:50 +0200

    Added PartnerController with Create and GetById

---
+++ Controllers/PartnerController.cs
@@ -0,0 +1,41 @@
+﻿using FaktureAPI.Data;
+using FaktureAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaktureAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartnerController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public PartnerController(ApplicationContext context) => _context = context;
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Partner>> Get() => await _context.Partners.ToListAsync();
+        
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Partner), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var partner = await _context.Partners.FindAsync();
+            return partner == null ? NotFound() : Ok(partner);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Partner partner)
+        {
+            await _context.Partners.AddAsync(partner);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = partner.Id }, partner);
+        }
+
+    }
+}
