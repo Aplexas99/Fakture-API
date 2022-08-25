0e912b2d338f930b54b7ffdf05e9f7b54a300fc6
Aplexas99 <toni_dzoic@hotmail.com>
2022-08-25 02:08:43 +0200

    Added IBillBodyRepository

---
+++ Repository/IBillBodyRepository.cs
@@ -0,0 +1,14 @@
+﻿using FaktureAPI.Models;
+
+namespace FaktureAPI.Repository
+{
+    public interface IBillBodyRepository: IRepository<BillBody>
+    {
+        Task<IEnumerable<BillBody>> GetAll();
+        Task<BillBody> GetById(int id);
+        void Create(BillBody owner);
+        void Update(BillBody owner);
+        void Delete(BillBody owner);
+
+    }
+}
