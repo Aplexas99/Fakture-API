95f7fd3f152ff6924bcca33c381e51651d70caf1
Aplexas99 <toni_dzoic@hotmail.com>
2022-08-25 02:19:10 +0200

    Added BillBodyRepository

---
+++ Repository/BillBodyRepository.cs
@@ -0,0 +1,40 @@
+﻿using FaktureAPI.Data;
+using FaktureAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaktureAPI.Repository
+{
+    public class BillBodyRepository : RepositoryBase<BillBody>, IBillBodyRepository
+    {
+        public BillBodyRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+        }
+
+        public async Task<IEnumerable<BillBody>> GetAll()
+        {
+            return await FindAll()
+                .OrderBy(b => b.Id)
+                .ToListAsync();
+
+        }
+
+        public async Task<BillBody> GetById(int id)
+        {
+            return await FindByCondition(b => b.Id.Equals(id))
+            .FirstOrDefaultAsync();
+        }
+
+        public void CreateBillBody(BillBody billBody)
+        {
+            Create(billBody);
+        }
+        public void UpdateBillBody(BillBody billBody)
+        {
+            Update(billBody);
+        }
+        public void DeleteBillBody(BillBody billBody)
+        {
+            Delete(billBody);
+        }
+    }
+}
