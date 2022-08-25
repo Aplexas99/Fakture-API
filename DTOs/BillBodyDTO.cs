3f2c1fd9c33517d5e75c956ebe9812334527c96c
Aplexas99 <toni_dzoic@hotmail.com>
2022-08-25 03:07:38 +0200

    Added BillBodyDTO

---
+++ DTOs/BillBodyDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace FaktureAPI.DTOs
+{
+    public class BillBodyDTO
+    {
+
+        public string Opis { get; set; }
+        public int Kolicina { get; set; }
+        public decimal CijenaDeviza { get; set; }
+        public decimal CijenaKm { get; set; }
+
+        public decimal? Rabat { get; set; }
+
+        public string? IznosRabata { get; set; }
+        public decimal? PDV { get; set; }
+        public string Vrijednost { get; set; }
+
+
+    }
+}
