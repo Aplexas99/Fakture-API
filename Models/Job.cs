2e5802d402c7c353ffa570a08a8663b69f4b9347
Aplexas99 <toni_dzoic@hotmail.com>
2023-01-19 16:57:41 +0100

    Added Job Model

---
+++ Models/Job.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FaktureAPI.Models
+{
+    public class Job
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Naziv { get; set; }
+        [Required]
+        public decimal CijenaDeviza { get; set; }
+        [Required]
+        public decimal CijenaKm { get; set; }
+
+    }
+}
