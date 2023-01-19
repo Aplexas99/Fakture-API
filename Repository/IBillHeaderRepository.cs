ede5dac3929b68b7397388f4396bf10cd188c3e6
Aplexas99 <toni_dzoic@hotmail.com>
2023-01-19 14:03:19 +0100

    Added BillHeader repo interface

---
+++ Repository/IBillHeaderRepository.cs
@@ -0,0 +1,13 @@
+﻿using FaktureAPI.Models;
+
+namespace FaktureAPI.Repository
+{
+    public interface IBillHeaderRepository : IRepository<BillHeader>
+    {
+        Task<IEnumerable<BillHeader>> GetAll();
+        Task<BillHeader> GetById(int id);
+        void Create(BillHeader owner);
+        void Update(BillHeader owner);
+        void Delete(BillHeader owner);
+    }
+}
