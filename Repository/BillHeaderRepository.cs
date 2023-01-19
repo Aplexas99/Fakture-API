9247a821374f17862f0d98c2b09517d0ae8a7073
Aplexas99 <toni_dzoic@hotmail.com>
2023-01-19 14:09:08 +0100

    Added BillHeader repo

---
+++ Repository/BillHeaderRepository.cs
@@ -0,0 +1,42 @@
+﻿using FaktureAPI.Data;
+using FaktureAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaktureAPI.Repository
+{
+    public class BillHeaderRepository : RepositoryBase<BillHeader>, IBillHeaderRepository
+    {
+
+        public BillHeaderRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<BillHeader>> GetAll()
+        {
+            return await FindAll()
+                .OrderBy(b => b.Id)
+                .ToListAsync();
+
+        }
+
+        public async Task<BillHeader> GetById(int id)
+        {
+            return await FindByCondition(b => b.Id.Equals(id))
+            .FirstOrDefaultAsync();
+        }
+
+        public void CreateBillHeader(BillHeader billHeader)
+        {
+            Create(billHeader);
+        }
+        public void UpdateBillHeader(BillHeader billHeader)
+        {
+            Update(billHeader);
+        }
+        public void DeleteBillHeader(BillHeader billHeader)
+        {
+            Delete(billHeader);
+        }
+    }
+}
